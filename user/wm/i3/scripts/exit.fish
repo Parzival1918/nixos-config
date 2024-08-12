111052e5c1b4836411d2a70d8852bb1e2495313d
Pedro Juan Royo <pedro.juan.royo@gmail.com>
2024-08-12 10:20:25 +0100

    add reboot option to exit shell script

--- user/wm/i3/scripts/exit.fish
+++ user/wm/i3/scripts/exit.fish
@@ -1,4 +1,4 @@
-set ans (echo "<b>Cancel</b>|<b><span color='orange'>Exit</span></b>|<b><span color='red'>Shutdown</span></b>" | rofi -sep '|' -location 3 -markup-rows -dmenu -p "Do you want to exit?" -l 3 -theme-str 'window {width: 6%; x-offset: -9px; y-offset: 53px; padding: 0; margin: 0;}' -theme-str 'inputbar { enabled: false;}' -theme-str 'listview {border: 0; margin: 0; padding: 0;}'  -format 'i')
+set ans (echo "<b>cancel</b>|<b><span color='orange'>exit</span></b>|<b><span color='red'>shutdown</span></b>|<b><span color='red'>reboot</span></b>" | rofi -sep '|' -location 3 -markup-rows -dmenu -p "Do you want to exit?" -l 4 -theme-str 'window {width: 6%; x-offset: -9px; y-offset: 53px; padding: 0; margin: 0;}' -theme-str 'inputbar { enabled: false;}' -theme-str 'listview {border: 0; margin: 0; padding: 0;}'  -format 'i')
 
 if test "$ans" = "0"
   echo "CANCEL"
@@ -6,4 +6,6 @@ else if test "$ans" = "1"
   i3-msg exit
 else if test "$ans" = "2"
   shutdown -h now
+else if test "$ans" = "3"
+  reboot
 end
